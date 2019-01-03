1193ada04bafada108d060fffd0cb81a8a04908d
Florin Onciu <florin.cosmin@fastmail.com>
2019-01-03 21:41:47 +0100

    simple server

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,9 @@ import * as http from 'http'
 const hostname = '127.0.0.1'
 const port = 3000
 
-const server = http.createServer((req, res) => {
+const server = http.createServer()
+
+server.on('request', (req: http.IncomingMessage, res: http.ServerResponse) => {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/plain')
   res.end('Hello World\n')
